10b9d70688e5d86b8a87fb47bfc7d87332e3b194
Gazaunga <Gazaunga@gmail.com>
2017-03-09 17:57:50 -0500

    Update i3.sh

--- bin/i3.sh
+++ bin/i3.sh
@@ -3,6 +3,10 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 pacaur -S xorg-xinit i3-gaps-git i3blocks-git gdm dmenu rofi emacs polkit-gnome termite compton gsimplecal polybar thunar gvfs file-roller thunar-archive-plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs libreoffice scrot feh moc smplayer smtube smplayer-themes smplayer-skins howdoi openshot glances leafpad gnome-tweak-tool rxvt-unicode-256color fontforge krita pandoc typora xfce4-taskmanager mpv texlive-most cmake darktable
 cd ~/Pictures
 wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-166605.jpg
+cd ~/
+wget https://github.com/bcicen/ctop/releases/download/v0.4/ctop-0.4-linux-amd64 -O ctop
+sudo mv ctop /usr/local/bin/
+sudo chmod +x /usr/local/bin/ctop
 sudo systemctl enable gdm.service
 reboot
 
