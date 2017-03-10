a1d088205aab46f0c11d6e7a390a6fac25140d4d
Gazaunga <Gazaunga@gmail.com>
2017-03-09 21:09:12 -0500

    Update i3.sh

--- bin/i3.sh
+++ bin/i3.sh
@@ -1,6 +1,6 @@
 sudo pacman -Syu --noconfirm -- needed
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-pacaur -S xorg-xinit i3-gaps-git i3blocks-git gdm dmenu rofi emacs polkit-gnome termite compton gsimplecal polybar thunar gvfs file-roller thunar-archive-plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs libreoffice scrot feh moc smplayer smtube smplayer-themes smplayer-skins howdoi openshot glances leafpad gnome-tweak-tool rxvt-unicode-256color fontforge krita pandoc typora xfce4-taskmanager mpv texlive-most cmake darktable
+pacaur -S xorg-xinit i3-gaps-git i3blocks-git gdm dmenu rofi emacs polkit-gnome termite compton gsimplecal polybar thunar gvfs file-roller thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler raw-thumbnailer gvfs-smb sshfs libreoffice scrot feh moc smplayer smtube smplayer-themes smplayer-skins howdoi openshot glances leafpad gnome-tweak-tool rxvt-unicode fontforge krita pandoc typora xfce4-taskmanager mpv texlive-most cmake darktable
 cd ~/Pictures
 wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-166605.jpg
 cd ~/
@@ -19,3 +19,5 @@ reboot
 ## xdg-user-dirs before moving wallpaper
 
 ## what else?
+
+#thunar-shares-plugin 
