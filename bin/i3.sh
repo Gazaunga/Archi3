07bd57037bbe3f6d8cbb6aaf34722b5f8275e711
Gazaunga <Gazaunga@gmail.com>
2017-03-09 20:27:36 -0500

    Update i3.sh

--- bin/i3.sh
+++ bin/i3.sh
@@ -7,6 +7,8 @@ cd ~/
 wget https://github.com/bcicen/ctop/releases/download/v0.4/ctop-0.4-linux-amd64 -O ctop
 sudo mv ctop /usr/local/bin/
 sudo chmod +x /usr/local/bin/ctop
+sudo chmod +x ~/Archi3/bin/SymLink.sh
+sh ~/Archi3/bin/SymLink.sh
 sudo systemctl enable gdm.service
 reboot
 
