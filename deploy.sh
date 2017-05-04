3b916235b2300a8f2a78613ec083d3fb33c2c2f3
Gazaunga <Gazaunga@gmail.com>
2017-05-04 16:02:39 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -28,56 +28,71 @@ ask() {
 
 dir=`pwd`
 
-if ask "Install symlink for .zshrc?" Y; then
-  ln -sf ${dir}/.zshrc ${HOME}/.zshrc
-  ln -sf ${dir}/.aliases.sh ${HOME}/.aliases.sh
+if ask "Install symlink for gsimplecal?" Y; then
+  ln -sf ${dir}/gsimplecal ${HOME}/gsimplecal
 fi
 
-if ask "Install symlink for .Xresources?" Y; then
-  ln -sf ${dir}/.Xresources ${HOME}/.Xresources
+if ask "Install symlink for .config?" Y; then
+  ln -sf ${dir}/.config ${HOME}/.config
 fi
 
-if ask "Install symlink for .compton.conf?" Y; then
-  ln -sf ${dir}/.compton.conf ${HOME}/.compton.conf
+if ask "Install symlink for .ssh?" Y; then
+  ln -sf ${dir}/.ssh ${HOME}/.ssh
 fi
 
-if ask "Install symlink for .vimrc?" Y; then
-  ln -sf ${dir}/.vimrc ${HOME}/.vimrc
+if ask "Install symlink for rofi?" Y; then
+  ln -sf ${dir}/rofi ${HOME}/rofi
 fi
 
-if ask "Install symlink for git?" Y; then
-  ln -sf ${dir}/.gitconfig ${HOME}/.gitconfig
+if ask "Install symlink for terminator?" Y; then
+  ln -sf ${dir}/terminator ${HOME}/terminator
 fi
 
-if ask "Install symlink for .xinitrc?" Y; then
-  ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
+if ask "Install symlink for .Xdefaults?" Y; then
+  ln -sf ${dir}/.Xdefaults ${HOME}/.Xdefaults
 fi
 
-if ask "Install symlink for bin?" Y; then
-  ln -sfn ${dir}/bin ${HOME}/bin
+if ask "Install symlink for .bashrc?" Y; then
+  ln -sfn ${dir}/.bashrc ${HOME}/.bashrc
 fi
 
 if ask "Install symlink for i3?" Y; then
-  ln -sfn ${dir}/.config/i3 ${HOME}/.config/i3
+  ln -sfn ${dir}/.profile ${HOME}/.profile
 fi
 
-if ask "Install symlink for .i3blocks.conf?" Y; then
-  ln -sf ${dir}/.config/i3blocks ${HOME}/.config/i3blocks
+if ask "Install symlink for .bash_aliases?" Y; then
+  ln -sf ${dir}/.bash_aliases ${HOME}/.bash_aliases
 fi
 
-if ask "Install symlink for dunst?" Y; then
-  ln -sfn ${dir}/.config/dunst ${HOME}/.config/dunst
+if ask "Install symlink for .bash_functions?" Y; then
+  ln -sfn ${dir}/.bash_functions ${HOME}/.bash_functions
 fi
 
-if ask "Install symlink for gtk-2.0?" Y; then
-  ln -sfn ${dir}/.config/gtk-2.0 ${HOME}/.config/gtk-2.0
-  ln -sfn ${dir}/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
+if ask "Install symlink for .fehbg?" Y; then
+  ln -sfn ${dir}/.fehbg ${HOME}/.fehbg
 fi
 
-if ask "Install symlink for gtk-3.0?" Y; then
-  ln -sfn ${dir}/.config/gtk-3.0 ${HOME}/.config/gtk-3.0
+if ask "Install symlink for .interrobangrc?" Y; then
+  ln -sfn ${dir}/.interrobangrc ${HOME}/.interrobangrc
 fi
 
-if ask "Install symlink for .config/termite?" Y; then
-  ln -sfn ${dir}/.config/termite ${HOME}/.config/termite
+if ask "Install symlink for .lesskey?" Y; then
+  ln -sfn ${dir}/.lesskey ${HOME}/.lesskey
 fi
+
+if ask "Install symlink for .screenrc" Y; then
+  ln -sfn ${dir}/.screenrc ${HOME}/.screenrc
+fi
+
+if ask "Install symlink for .xbindkeysrc" Y; then
+  ln -sfn ${dir}/.xbindkeysrc ${HOME}/.xbindkeysrc
+fi
+
+if ask "Install symlink for .xinitrc" Y; then
+  ln -sfn ${dir}/.xinitrc ${HOME}/.xinitrc
+fi
+mv -f wallhaven-59918.jpg ~/wallhaven-59918.jpg
+if ask "Install symlink for bin" Y; then
+  ln -sfn ${dir}/bin ${HOME}/bin
+fi
+
